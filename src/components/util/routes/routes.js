8c67148961254a61f1e843e0a2651a8e76fc33d6
xvvas <miguelvg097@gmail.com>
2024-07-01 14:59:57 -0500

    Create Bio page and assing route

--- src/components/util/routes/routes.js
+++ src/components/util/routes/routes.js
@@ -1,8 +1,9 @@
 import Home from "@ce/landing/Home";
-
+import Bio from "@ce/landing/Bio"
 
 const routes = [
     { path: "/", name: "", element: Home },
+    { path: "/Bio", name: "Bio", element:Bio }
 ]
 
 export default routes;
