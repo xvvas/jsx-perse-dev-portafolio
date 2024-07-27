67d75f6cd42d6e2db9e4582689c0de4c1b92ded8
xvvas <miguelvg097@gmail.com>
2024-07-27 17:42:42 -0500

    Add 'Work' page to the 'Routes' directory

--- src/components/util/routes/routes.js
+++ src/components/util/routes/routes.js
@@ -1,9 +1,12 @@
 import Home from "@ce/landing/Home";
 import Bio from "@ce/landing/Bio"
+import Work from "@ce/landing/Work"
 
 const routes = [
     { path: "/", name: "", element: Home },
-    { path: "/Bio", name: "Bio", element:Bio }
+    { path: "/Bio", name: "Bio", element:Bio },
+    { path: "/Work", name: "Work", element:Work }
+
 ]
 
 export default routes;
