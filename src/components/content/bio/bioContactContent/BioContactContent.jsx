32fe114bf8787bccb0024caf23e7a001cc1d6d84
xvvas <miguelvg097@gmail.com>
2024-07-27 17:46:40 -0500

    replace the class nomenclature in BioContactContent from ‘bioContactContent_content’ to ‘bioContactContent__content’ on line 11

--- src/components/content/bio/bioContactContent/BioContactContent.jsx
+++ src/components/content/bio/bioContactContent/BioContactContent.jsx
@@ -8,7 +8,7 @@ import { IconFigma, IconGitHub, IconInstagram, IconLinkedIN } from "@cu/icons/Ic
 export default function BioContactContent() {
     return (
         <ContainerSection className="BioContactContent">
-            <div className="bioContactContent_content">
+            <div className="bioContactContent__content">
                 <ContainerCard className="bioContactContent__card">
                     <TypeSubTitle1 className="bioContactContent__cardTitle">Redes Sociales</TypeSubTitle1>
                     <TypeBody2 className="bioContactContent__cardTxt">Ponte en contacto conmigo a través de las redes sociales.</TypeBody2>
