bf2a33fd9e42b3786b38d3525528679d7a902610
xvvas <miguelvg097@gmail.com>
2024-08-19 16:59:03 -0500

    add  'WorkDescriptionContent' and css style, create 'WorkProjectsContent' files

--- src/components/pages/landing/Work.jsx
+++ src/components/pages/landing/Work.jsx
@@ -1,9 +1,11 @@
+import WorkDescriptionContent from "@cc/work/workDescriptionContent/WorkDescriptionContent";
 import WorkMainContent from "@cc/work/workMainContent/WorkMainContent";
 
 export default function Home() {
     return (
         <>
             <WorkMainContent/>
+            <WorkDescriptionContent/>
         </>
     )
 }
