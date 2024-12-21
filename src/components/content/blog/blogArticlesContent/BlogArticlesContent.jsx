ef6a6009815cc2b5c6a81cf3703b2a988b584b9e
xvvas <miguelvg097@gmail.com>
2024-12-21 11:11:11 -0500

    Add blog content components

---
+++ src/components/content/blog/blogArticlesContent/BlogArticlesContent.jsx
@@ -0,0 +1,12 @@
+
+import { ContainerSection } from "@cu/containers/Containers";
+import { useEffect, useState } from "react";
+
+export default function BlogArticlesContent(){
+
+    return(
+        <ContainerSection>
+            
+        </ContainerSection>
+    )
+} 
